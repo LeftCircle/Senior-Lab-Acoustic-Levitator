dd3c62ace8e0fdd3f8bc0912a1b467e62a32ba33
LeftAreola <47573732+LeftAreola@users.noreply.github.com>
2019-02-26 11:52:32 -0500

    Update rotated_mesh.py

--- rotated_mesh.py
+++ rotated_mesh.py
@@ -8,46 +8,57 @@ from mpl_toolkits.mplot3d import Axes3D
 
 
 class Rotated_mesh:
-    def __init__(self, tr_i, z_middle, h_i, a_i): 
+    def __init__(self, tr_i, z_middle, h_i, a_1, bottom, top): 
         self.tr_i = tr_i #transisters in i
         self.z_middle = z_middle #midpoint of TinyLev
         self.h_i = h_i #height of ring i  == z from transducers_ring1
-        self.a_i = a_i #radius of spherical cap at ring i
-        
+        self.a_1 = a_1 #radius of spherical cap at ring i -----------------------can probably be removed
+        #self.bottom = bottom ; self.top = top #1 for true, 0 for false
+    
+            
     def radius_sphere(self):
-        rSphere = (self.a1**2 + self.h1**2) / (2 * self.h1)
+        rSphere = (self.a1**2 + self.h1**2) / (2 * self.h1)  #-> will be used to calculate actual values to replace h_i and a_i
         return rSphere
-       
-    def theta(self):                                #probably being calculated wrong
-        angle = np.arctan(self.a_i / self.z_middle)
+    
+    def alpha(self): #transducer xy angle in polar
+        angle =[]
+        for i in range(self.tr_i):
+            angle.append(2 *np.pi / self.tr_i * (i))
+        return angle
+##################################################################################
+#if we can measure a_i and h_i then we can get rid of this mess and generalize
+#for spherical caps
+        
+    def a_i(self):
+        return (( 10 / (self.alpha()[1]))*1.5)    
+  
+    def theta(self):                                
+        angle = np.arctan(self.a_i() / (self.z_middle - self.h_i))
         return angle
         
     
     def unrotated_xyz_i(self, transducer_array, i):
         xyz = [transducer_array[0][i], transducer_array[1][i],
                transducer_array[2][i]]
-        return xyz
-    def aprox_a_i(self):
-        return (2. * 5 / (2 * np.pi / (self.tr_i)))*1.5
-    
+        return xyz  
     
     def rotated_mesh(self):
         rotate = matrix_rotation.Rotation(1)
         #grabbing one ring at a time
-        trans = transducers_ring.Transducers(self.tr_i, 5, self.h_i, 25, 0)  #adjusting this cuts down time by a lot
+        trans = transducers_ring.Transducers(self.tr_i, 5, 0, 10, 0)  #adjusting this cuts down time by a lot
         trans = trans.transducer()
         
         #rotating each transducers in ring x:
         rotated_array = []
         for i in range(self.tr_i):
-            #rotate everything by pi/4 as test
-            alpha = 2 *np.pi / self.tr_i * (i) ########might not be plus 1
             transducer_array = self.unrotated_xyz_i(trans, i)
-            ry = rotate.rotation_y(transducer_array, -self.theta())
-            rx = rotate.rotation_z(ry, alpha)
+            ry = rotate.rotation_y(transducer_array, -self.theta())   #blows up here! should do all rotations at 0, 0
+            rx = rotate.rotation_z(ry, self.alpha()[i])
             #now translate
-            rx[0] += self.aprox_a_i() * np.cos(alpha)
-            rx[1] += self.aprox_a_i() * np.sin(alpha)
+            rx[0] += self.a_i() * np.cos(self.alpha()[i]) #was aprox_a_i 
+            rx[1] += self.a_i() * np.sin(self.alpha()[i]) 
+            #and vertical translation
+            rx[2] += self.h_i
             
             rotated_array.append(rx)
             
@@ -58,7 +69,11 @@ class Rotated_mesh:
     
     
     #rotate by theta then alpha(radians to center) then translate
-        
+     
+    #to get a better image, ensure that the radius of each ring is 
+    #determined by the spherical cap
+    
+    
 '''
 data is stored as
 [transducer i][0 = x, 1 = y, 2 = z]
@@ -68,14 +83,15 @@ fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
 ax.set_xlim3d(-30, 30)
 ax.set_ylim3d(-30, 30)
-ax.set_zlim3d(-30, 30)
-N = [6,12,18]
-Z = [1 , 5, 10]
-for n in N:
-    rot = Rotated_mesh(n, 50, n / 3 , 10)
+ax.set_zlim3d(-5, 210)
+N = [ 6, 12, 18, 6, 12, 18]
+Z = [0, 15, 25, 200, 185, 175]
+Z1 = [1, 5, 10]
+for i in range(len(N)):
+    rot = Rotated_mesh(N[i], 100, Z[i] , 0, 1, 0)  
     rot = rot.rotated_mesh()
 
-    for i in range(n):
+    for i in range(N[i]):
         x = rot[i][0]
         y = rot[i][1]
         z = rot[i][2]
