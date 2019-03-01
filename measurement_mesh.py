edf3c907d2fc6330ee63b2e7352892edf5fec6fe
bshugg <48027952+bshugg@users.noreply.github.com>
2019-03-01 15:01:20 -0500

    Add files via upload

---
+++ measurement_mesh.py
@@ -0,0 +1,122 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+#m_meshpoints
+#rotated mesh
+import numpy as np
+import pylab as py
+import matplotlib.pyplot as plt
+import transducers_ring
+import matrix_rotation
+import rotated_mesh
+from mpl_toolkits.mplot3d import Axes3D
+
+# mesh of measurement points in between the transducer rings
+class M_mesh:
+    def __init__(self, radius_largest_ring, h_largest_ring, z_middle, m_mesh_n):
+        self.radius_largest_ring = radius_largest_ring
+        self.m_mesh_n = m_mesh_n #meshpoints on one axis of rectangle defining mesh between top and bottom transducers
+        self.h_largest_ring = h_largest_ring
+        self.z_middle = z_middle
+    def m_mesh(self):
+        x_start = -self.radius_largest_ring
+        x_end   =  self.radius_largest_ring
+           
+        y_start = x_start
+        y_end   = x_end #b/c we are making a square base
+        
+        
+        #mesh point created as a square
+        x = np.linspace(x_start, x_end, self.m_mesh_n) 
+        y = np.linspace(y_start, y_end, self.m_mesh_n)
+        x, y = np.meshgrid(x, y) #temp values to get proper size
+        
+        #defining points inside the radius, and only keeping those
+        # creates circular mesh
+        r = np.sqrt(x**2 + y**2)
+        inside = r < self.radius_largest_ring
+        #making x, y, z same length
+        x_inside = x[inside]
+        y_inside = y[inside]
+        
+        # translating circular mesh up to create 3-D mesh
+        z_start = self.h_largest_ring + 6 #plus 6 ensures not touching mesh transducers
+        z_end   = 2 * self.z_middle - self.h_largest_ring - 6
+        z = np.linspace(z_start, z_end, int(len(x_inside) / 2))
+        
+        # arrays of 3-D mesh points
+        x_array = []
+        y_array = [] 
+        z_array = []
+        for i in range(int(len(x_inside) / 2)):
+            x_array.append(x_inside)
+            y_array.append(y_inside)
+            z_array.append(np.ones(len(x_inside)) * z[i])
+            
+        
+        
+        
+        
+        
+        
+        ############# np.meshgrid may cause interesting indexing of xyz########
+        return x_array, y_array, z_array
+ 
+'''
+data is accessed by
+[0 = x, 1 = y, 2 = z][array of points]
+The mesh is not necessarily uniform, but it is still a mesh
+'''
+
+#testing
+#array sizes need to be the same
+mesh = M_mesh(10, 10, 50, 20)
+m = mesh.m_mesh()
+#print(m)  #([x,y,z][len(x,y,z)][len(x,y,z)])
+print(len(m[1][0]))
+print(len(m[2][0]))
+
+
+# create figure for plotting
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+ax.set_xlim3d(-100, 100)
+ax.set_ylim3d(-100, 100)
+ax.set_zlim3d(-5, 210)
+
+
+for i in range(len(m[1][0])):
+    ax.scatter(m[0][i], m[1][i], m[2][i])
+
+
+
+
+# x,y,z points of transducer rings
+for i in range(len(transducers_ring.ring_point()[0])):
+    ax.scatter(rotated_mesh.x,)
+
+plt.show()    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- transducers_ring.py
+++ transducers_ring.py
@@ -17,52 +17,78 @@ discretize transducer and reflector into small cells
 #radius should be 5mm and distance between ~2.5 mm
 class Transducers:
     #just creating the transducers on z for now
-    def __init__(self, tr1, radius,
+    def __init__(self, ntr, radius,
                  z, meshN, yshift):
         #self.centerx = centerx
         #self.centery = centery       
         self.radius = radius
         self.meshN = meshN
         #self.distance_b_transducers = distance_b_transducers
-        self.tr1 = tr1  #transducers in rx
+        self.ntr = ntr  #transducers in rx
         self.z = z
         self.yshift = yshift
+    
     #Transducers in inner ring (ring1)
     def transducer_angle(self, tr):
         angle = 2 * np.pi / (tr)
         return angle
+    
+    
+    # creates circular mesh
+    def ring_points(self):
+        centerx = 0      #radius_ring1 * np.cos(self.transducer_angle(self.ntr)*i)
+        centery = 0      #radius_ring1 * np.sin(self.transducer_angle(self.ntr)*i) - (self.yshift) #quick fix for positioning
+                            
+        x_start = centerx - self.radius
+        x_end   = centerx + self.radius
+           
+        y_start = centery - self.radius
+        y_end   = centery + self.radius
+        #mesh point created as a square
+        x = np.linspace(x_start, x_end, self.meshN) 
+        y = np.linspace(y_start, y_end, self.meshN)
+        
+        x, y = np.meshgrid(x, y)
+        
+        #defining points inside the radius, and only keeping those
+        r = np.sqrt((x - centerx)**2 + (y - centery)**2)
+        inside = r < self.radius 
+        
+        return x[inside], y[inside]
+    
+    # returns mesh (x,y,z) coordinates of all of the transducers in one ring
+    # coordinate][number of transducer]
+    # 0 = x, 1 = y, 2=z
     def transducer(self):  # 3D
         #create a square mesh and only pick values that are in circle
+        
+        # calls function that creates circular mesh
+        xy = self.ring_points()
+        
+        x = xy[0]
+        y = xy[1]
+        
+        print("x",x)
+        print("y",y)
+                
+        # array for z values
+        # all zeros initially before translation
+        height = np.zeros(len(x))
+        
         #holds x and y values of transducer meshes
         transducer_i_x = []
         transducer_i_y = []
         transducer_i_z = []
-                
+        
         #creating meshpoints for y axis transducers
-        for i in range(0, self.tr1): 
-            #radius_ring1 = (2. * self.radius / (self.transducer_angle(self.tr1)))*1.5
-            centerx = 0#radius_ring1 * np.cos(self.transducer_angle(self.tr1)*i)
-            centery = 0#radius_ring1 * np.sin(self.transducer_angle(self.tr1)*i) - (self.yshift) #quick fix for positioning
-                                
-            x_start = centerx - self.radius
-            x_end   = centerx + self.radius
-               
-            y_start = centery - self.radius
-            y_end   = centery + self.radius
-            #mesh point created as a square
-            x = np.linspace(x_start, x_end, self.meshN) 
-            y = np.linspace(y_start, y_end, self.meshN)
-            
-            x, y = np.meshgrid(x, y)
-            
-            #defining points inside the radius, and only keeping those
-            r = np.sqrt((x - centerx)**2 + (y - centery)**2)
-            inside = r < self.radius 
-            
-            height = np.zeros(len(x[inside]))
+        for i in range(0, self.ntr): 
+            #radius_ring1 = (2. * self.radius / (self.transducer_angle(self.ntr)))*1.5
+                       
             #height[:] += self.z
-            transducer_i_x.append(x[inside])
-            transducer_i_y.append(y[inside])
+            
+            # creation of ntr number of transducers at the origin
+            transducer_i_x.append(x)
+            transducer_i_y.append(y)
             transducer_i_z.append(height)
         
             #for graphing one transducer
@@ -78,7 +104,8 @@ class Transducers:
         
         return transducer_i_x, transducer_i_y, transducer_i_z
     
-    
+
+
 
 #for testing    
 #how to access data :
@@ -96,16 +123,13 @@ N = [6,12,18]
 for n in N:
     
     all_t = Transducers(n, 5, 0, 50, 0)
-
     all_t = all_t.transducer()
-    #[0 = x, 1 = y][transducer_i]
+    #[0 = x, 1 = y, 2=z][transducer_i]
     x = all_t[0][0]  
     y = all_t[1][0]  
-
     
     for i in range(n):  
         ax.scatter(all_t[0][i], all_t[1][i]) 
-
 plt.show()  
 '''
 '''
@@ -120,3 +144,10 @@ ax.scatter(x, y)
 plt.show()
 '''
 
+
+
+
+
+
+
+print("transducers_ring - Done.")
