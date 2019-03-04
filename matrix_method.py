85eb6f7b4323e7a41787af61abf2fb66708d5b4c
bshugg <48027952+bshugg@users.noreply.github.com>
2019-03-04 13:48:16 -0500

    Update matrix_method.py

--- matrix_method.py
+++ matrix_method.py
@@ -10,13 +10,19 @@ from mpl_toolkits.mplot3d import Axes3D
 
 class Matrix_method:
     def __init__(self, omega, c, amplitude, t_meshN, t_radius, 
-                 excitation_phase_n):
+                 excitation_phase_n, density, wavelength):
         self.omega = omega #anglular freq???
         self.c = c
-        self.amplitude = amplitude
+        self.amplitude = amplitude # amplitude of displacement
         self.t_meshN = t_meshN
         self.t_radius = t_radius
-        self.excitation_phase_n = excitation_phase_n
+        self.excitation_phase_n = excitation_phase_n # phase of displacement 
+        self.density = density
+        self.wavelength = wavelength
+        
+        # TODO: find out what amplitude and excitatin_phase_n actually are
+        
+    
     '''
     note - we don't have to calculate both the top force from both the top and
     the bottom. We actually just need to calculate the force from the bottom
@@ -39,7 +45,8 @@ class Matrix_method:
     def sn(self): #radius of transducer cell
         return(self.t_radius / self.t_meshN)**2
     
-    
+    # calculates distance between transducer points (n,t) and measurement points (m)
+    # t_points, m_points are 2-D arrays of x,y,z points
     def r_nm_m(self, t_points, m_points): #r_nm matrix
         m_length = len(m_points[0]) ; t_length = len(t_points[0])
         r_nm_matrix = np.zeros([m_length, t_length], dtype=complex) ###making complex array
@@ -51,15 +58,17 @@ class Matrix_method:
                            (m_points[2][i]-t_points[2][k])**2) 
         return r_nm_matrix
     
+    # element of transfer matrix between transducer points (n) and measurement points (m)
     def t_nm(self, r_nm): #t_nm^TM from matrix method paper
         k = self.omega / self.c
         # I think sqrt(-1) = 1j
         return self.sn() * (np.exp(-1j*k*r_nm)) / r_nm
     
+    # element of displacement of each cell s_n
     def u_n(self):
         return self.amplitude * np.exp(1j * self.excitation_phase_n) 
         
-
+    # transfer matrix between transducer points (n) and measurement points (m)
     def t_matrix(self, t_points, m_points):
         m_length = len(m_points[0]) ; t_length = len(t_points[0])
         print(m_length, 'm_length', t_length, 't_length')
@@ -72,6 +81,7 @@ class Matrix_method:
                 #potentially make everything a complex number to account for it?
         return t_m
     
+    # displacement matrix
     def u_matrix(self, t_points):
         t_length = len(t_points[0])
         print(t_length, 't_length')
@@ -80,7 +90,15 @@ class Matrix_method:
             u_mat[i] = self.u_n()
         return u_mat
             
-    
+    # pressure matrix at measurement points (m)
+    def p_matrix(self, t_mat, u_mat):
+        
+        p_prefactor = self.omega * self.rho * self.c / self.lambda
+        
+        # WANT:
+        # p_prefactor * matrix multiplication of t_mat and u_mat
+        
+        
 
                 
             
